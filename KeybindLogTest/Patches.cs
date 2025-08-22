1a4299533141e805535cfe1d0c2a7c2aecb12099
creatrope <sendhb@gmail.com>
2025-08-22 14:31:23 -0700

    only one keybind works

--- KeybindLogTest/Patches.cs
+++ KeybindLogTest/Patches.cs
@@ -8,9 +8,9 @@ namespace KeybindLogTest
 {
     internal static class KeybindActions
     {
-        public static void OnF10() => Debug.Log("[KeybindLogTest] CTRL+F10 pressed");
-        public static void OnF11() => Debug.Log("[KeybindLogTest] CTRL+F11 pressed");
-        public static void OnF12() => Debug.Log("[KeybindLogTest] CTRL+F12 pressed");
+        public static void OnF10() => Debug.Log("[KeybindLogTestX] CTRL+F10 pressed");
+        public static void OnF11() => Debug.Log("[KeybindLogTestX] CTRL+F11 pressed");
+        public static void OnF12() => Debug.Log("[KeybindLogTestX] CTRL+F12 pressed");
     }
 
     internal sealed class KeybindHandler : IInputHandler
@@ -47,11 +47,11 @@ namespace KeybindLogTest
             manager.RegisterPatchClass(typeof(KeybindHandler));
             var actionManager = new PActionManager();
             F10Action = actionManager.CreateAction(
-                "KeybindLogTest.F10", "Test F10", new PKeyBinding(KKeyCode.F10, Modifier.Ctrl));
-            F11Action = actionManager.CreateAction(
-                "KeybindLogTest.F11", "Test F11", new PKeyBinding(KKeyCode.F11, Modifier.Ctrl));
+            "KeybindLogTest.F10", "Test F10", new PKeyBinding(KKeyCode.F10, Modifier.Ctrl | Modifier.Shift));
             F12Action = actionManager.CreateAction(
-                "KeybindLogTest.F12", "Test F12", new PKeyBinding(KKeyCode.F12, Modifier.Ctrl));
+                "KeybindLogTest.F12", "Test F12", new PKeyBinding(KKeyCode.F12, Modifier.Ctrl | Modifier.Shift));
+            F11Action = actionManager.CreateAction(
+                "KeybindLogTest.F11", "Test F11", new PKeyBinding(KKeyCode.F11, Modifier.Ctrl | Modifier.Shift));
         }
     }
 
