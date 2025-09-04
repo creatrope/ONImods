4cdaeae5eba05771ae5c28d7eca2af93469b799f
creatrope <sendhb@gmail.com>
2025-09-03 17:12:30 -0700

    fix logging from dev directory

--- HLib/Logger.cs
+++ HLib/Logger.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace HLib
@@ -12,10 +13,24 @@ namespace HLib
         public Logger(string modName)
         {
             this.modName = modName;
-            // Enable logging if the executable is running from a "Local" directory
+            // Enable logging if the executable path contains "/Dev/", "\Dev\", "/Local/", or "\Local\"
             string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
             string modDir = Path.GetDirectoryName(exePath);
-            isLoggingEnabled = modDir != null && modDir.IndexOf("Local", StringComparison.OrdinalIgnoreCase) >= 0;
+            isLoggingEnabled = false;
+            if (modDir != null)
+            {
+                // Split the path into directory names and check for "Dev" or "Local"
+                foreach (var part in modDir.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar))
+                {
+                    if (part.Equals("Dev", StringComparison.OrdinalIgnoreCase) ||
+                        part.Equals("Local", StringComparison.OrdinalIgnoreCase))
+                    {
+                        isLoggingEnabled = true;
+                        break;
+                    }
+                }
+            }
+            Debug.Log($"[{modName}] Logging is {(isLoggingEnabled ? "ENABLED" : "DISABLED")}.");
         }
 
         public void SetLoggingState(bool enabled)
